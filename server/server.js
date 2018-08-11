fd4a05efac8ef59870df26a8be716f4ee661cbf9
Adam King <adam@tvrbo.pro>
2018-08-11 22:53:48 +0200

    add api with first flickr endpoint

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const PORT = 4000;
 const app = express();
@@ -6,6 +7,9 @@ const webpackMiddleware = require("webpack-dev-middleware");
 const webpack = require("webpack");
 const config = require("../webpack.dev.config.js");
 const compiler = webpack(config);
+const axios = require("axios");
+// Replace with your Flickr Key
+const { FLICKR_KEY } = process.env;
 
 app.use(
   webpackMiddleware(compiler, {
@@ -13,6 +17,24 @@ app.use(
     publicPath: config.output.publicPath,
   }),
 );
+
+const router = express.Router();
+
+// Get first page of "interesting" images
+router.route("/images").get(function(req, res) {
+  const extras =
+    "description, date_upload, date_taken, owner_name, original_format, last_update, geo, tags, machine_tags, o_dims, views, media, url_sq, url_t, url_s, url_q, url_m, url_n, url_z, url_c, url_l, url_o";
+  axios
+    .get(
+      `https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=${FLICKR_KEY}&format=json&extras=${extras}&per_page=10`,
+    )
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(err => res.send(err));
+});
+
+app.use("/api", router);
 
 app.use("*", (req, res, next) => {
   const filename = path.join(compiler.outputPath, "index.html");
