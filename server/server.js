71b21d9761268ec9b0aafd8a224469da07e33e46
Adam King <adam@tvrbo.pro>
2018-08-12 22:30:04 +0200

    add page option to endpoint and get json response

--- server/server.js
+++ server/server.js
@@ -20,13 +20,14 @@ app.use(
 
 const router = express.Router();
 
-// Get first page of "interesting" images
+// Get "interesting" images (page 1 unless specified)
 router.route("/images").get(function(req, res) {
   const extras =
     "description, date_upload, date_taken, owner_name, original_format, last_update, geo, tags, machine_tags, o_dims, views, media, url_sq, url_t, url_s, url_q, url_m, url_n, url_z, url_c, url_l, url_o";
+  const page = req.query.page ? `&page=${req.query.page}` : "";
   axios
     .get(
-      `https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=${FLICKR_KEY}&format=json&extras=${extras}&per_page=10`,
+      `https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=${FLICKR_KEY}&format=json&extras=${extras}&per_page=10${page}&nojsoncallback=1`,
     )
     .then(response => {
       res.send(response.data);
