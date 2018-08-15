1d93b1376297357425f77ca281b64574e1c26c56
Adam King <adamking81@hotmail.com>
2018-08-15 11:37:01 +0200

    add lightbox component

---
+++ client/components/Lightbox/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+import { closeLightbox } from "../../actions";
+
+import "./style.css";
+
+class Lightbox extends Component {
+  renderPhoto = () => {
+    const { photo } = this.props;
+    return (
+      <div className="lightbox">
+        <div
+          className="close-button"
+          onClick={() => this.props.closeLightbox()}
+        >
+          <i className="material-icons">close</i>
+        </div>
+        <div className="container">
+          <div className="image-wrapper col s12">
+            <div
+              className="lightbox-img"
+              style={{ backgroundImage: `url(${photo.url_c})` }}
+            />
+          </div>
+          <div className="photo-info col s12">
+            {photo.title} by {photo.ownername}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  closeLightbox = () => this.props.closeLightbox();
+
+  render() {
+    return (this.props.isLightboxOpen && this.renderPhoto()) || null;
+  }
+}
+
+const mapStateToProps = state => ({
+  isLightboxOpen: state.gallery.isLightboxOpen,
+  photo: state.gallery.selectedPhoto,
+});
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ closeLightbox }, dispatch);
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Lightbox);
